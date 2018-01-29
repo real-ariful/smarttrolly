eca76019845ac3cb39740f2e8fff49c025c2628a
AZM Ariful Haque Real <33877999+realariful@users.noreply.github.com>
2018-01-29 11:01:10 +0600

    AVR
    AVR

---
+++ AVR_DataSoft_Iot/ArduinoSketch1/ArduinoSketch1/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 11/15/2017 4:03:16 PM
+ * Author : real
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
