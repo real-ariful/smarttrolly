c344aed36a0c1e7c7200b81a26d1ad8e364455be
AZM Ariful Haque Real <33877999+realariful@users.noreply.github.com>
2018-01-29 10:35:15 +0600

    AVR
    AVR

---
+++ AVR_DataSoft_Iot/Uno_read_write/Uno_read_write/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 11/16/2017 12:51:58 PM
+ * Author : real
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
