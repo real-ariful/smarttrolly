684d9847e586e390f8f34df65ca9571513ab87a2
AZM Ariful Haque Real <33877999+realariful@users.noreply.github.com>
2018-01-29 10:39:40 +0600

    AVR
    AVR

---
+++ AVR_DataSoft_Iot/Project/Project/Debug/main.d
@@ -0,0 +1,43 @@
+main.d main.o main.o: .././main.c \
+  c:/winavr-20071221/bin/../avr/include/avr/io.h \
+  c:/winavr-20071221/bin/../avr/include/avr/sfr_defs.h \
+  c:/winavr-20071221/bin/../avr/include/inttypes.h \
+  c:/winavr-20071221/bin/../avr/include/stdint.h \
+  C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.132\include/avr/iom328p.h \
+  c:/winavr-20071221/bin/../avr/include/avr/portpins.h \
+  c:/winavr-20071221/bin/../avr/include/avr/common.h \
+  c:/winavr-20071221/bin/../avr/include/avr/version.h \
+  c:/winavr-20071221/bin/../avr/include/avr/fuse.h \
+  c:/winavr-20071221/bin/../avr/include/avr/lock.h \
+  c:/winavr-20071221/bin/../avr/include/util/delay.h \
+  c:/winavr-20071221/bin/../avr/include/util/delay_basic.h \
+  c:/winavr-20071221/bin/../avr/include/stdlib.h \
+  c:\winavr-20071221\bin\../lib/gcc/avr/4.2.2/include/stddef.h
+
+c:/winavr-20071221/bin/../avr/include/avr/io.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/sfr_defs.h:
+
+c:/winavr-20071221/bin/../avr/include/inttypes.h:
+
+c:/winavr-20071221/bin/../avr/include/stdint.h:
+
+C:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.2.132\include/avr/iom328p.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/portpins.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/common.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/version.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/fuse.h:
+
+c:/winavr-20071221/bin/../avr/include/avr/lock.h:
+
+c:/winavr-20071221/bin/../avr/include/util/delay.h:
+
+c:/winavr-20071221/bin/../avr/include/util/delay_basic.h:
+
+c:/winavr-20071221/bin/../avr/include/stdlib.h:
+
+c:\winavr-20071221\bin\../lib/gcc/avr/4.2.2/include/stddef.h:
---
+++ AVR_DataSoft_Iot/Project/Project/main.c
@@ -0,0 +1,101 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+#define BAUDRATE 19200
+#define BAUD_PRESCALLER (((F_CPU / (BAUDRATE * 16UL))) - 1)
+#define VREF 5
+#define POT 10000
+
+//Declaration of our functions
+uint16_t adc_value;
+void USART_init(void);
+unsigned char USART_receive(void);
+void USART_send( unsigned char data);
+void USART_putstring(char* StringPtr);
+void USART_putInt(int number);
+
+void InitADC()
+{
+	// Select Vref=AVcc
+	ADMUX |= (1<<REFS0);
+	//set prescaller to 128 and enable ADC
+	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
+}
+uint16_t ReadADC(uint8_t ADCchannel)
+{
+	//select ADC channel with safety mask
+	ADMUX = (ADMUX & 0xF0) | (ADCchannel & 0x0F);
+	//single conversion mode
+	ADCSRA |= (1<<ADSC);
+	// wait until ADC conversion is complete
+	while( ADCSRA & (1<<ADSC) );
+	return ADC;
+}
+int main(void){
+	USART_init();        //Call the USART initialization code
+	InitADC();
+	DDRB = 0x01;
+	while(1)
+	{
+		
+		adc_value = ((ReadADC(0)-4)/2);
+		char buffer[4];
+		utoa(adc_value, buffer, 10);
+		USART_putstring(buffer);
+		_delay_ms(100);
+		
+		if (ReadADC(0) > 154)
+		{
+			PORTB =0x01;
+			_delay_ms(100);
+		}
+		else
+		{
+			PORTB = 0x00;;
+			_delay_ms(100);
+		}
+	}
+	return 0;
+}
+
+
+void USART_init(void)
+{
+	
+	UBRR0H = (uint8_t)(BAUD_PRESCALLER>>8);
+	UBRR0L = (uint8_t)(BAUD_PRESCALLER);
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	UCSR0C = (3<<UCSZ00);
+}
+
+unsigned char USART_receive(void)
+{
+	while(!(UCSR0A & (1<<RXC0)));
+	return UDR0;
+}
+
+void USART_send( unsigned char data)
+{
+	while(!(UCSR0A & (1<<UDRE0)));
+	UDR0 = data;
+	
+}
+
+void USART_putInt(int number)
+{
+	char buffer [sizeof(unsigned int)*8+1];
+	utoa (number,buffer,10);
+	USART_putstring(buffer);
+}
+
+void USART_putstring(char* StringPtr)
+{
+	
+	while(*StringPtr != 0x00){
+		USART_send(*StringPtr);
+	StringPtr++;}
+	USART_send('\r');
+	
+}
+
